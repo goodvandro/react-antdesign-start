bc1a9bba07c180f78bd0ee414aeabb3338d6a93f
Evandro Lima <evandro.lima@gat.digital>
2022-08-11 18:23:08 +0000

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { Col, Row } from 'antd';
+import { Col, Row, Typography } from 'antd';
 import './index.css';
+
+const { Title } = Typography;
 
 function App() {
   return (
     <div>
       <Row gutter={8} justify={'center'}>
-        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
-          <div className='Column'>coluna 1</div>
+        <Col span={12}>
+          <Title>h1. Ant Design</Title>
+          <Title level={2}>h2. Ant Design</Title>
+          <Title level={3}>h3. Ant Design</Title>
+          <Title level={4}>h4. Ant Design</Title>
+          <Title level={5}>h5. Ant Design</Title>
         </Col>
       </Row>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Breadcrumb, Layout, Menu } from 'antd';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css';
+
+const { Header, Content, Footer } = Layout;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <Header
+        style={{
+          position: 'fixed',
+          zIndex: 1,
+          width: '100%',
+        }}
+      >
+        <div className='logo' />
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          defaultSelectedKeys={['2']}
+          items={new Array(3)
+            .fill(null)
+            .map((_, index) => ({
+              key: String(index + 1),
+              label: `nav ${index + 1}`,
+            }))}
+        />
+      </Header>
+      <Content
+        className='site-layout'
+        style={{ padding: '0 50px', marginTop: 64 }}
+      >
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          className='site-layout-background'
+          style={{ padding: 24, minHeight: 380 }}
+        >
+          <App />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
   </React.StrictMode>
 );
 
