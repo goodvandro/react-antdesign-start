15310d30d4c6d547378790b28c37ec3692025c55
Evandro Lima <evandro.lima@gat.digital>
2022-08-11 18:01:31 +0000

    Responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,9 @@ import './index.css';
 function App() {
   return (
     <div>
-      <Row gutter={16} justify={'center'} align={'middle'}>
-        <Col span={4}>
-          <div className='Column'></div>
-          coluna 1
-        </Col>
-        <Col span={4}>
-          <div className='Column'></div>
-          coluna 2
-        </Col>
-        <Col span={8}>
-          <div className='Column'></div>
-          coluna 3
-        </Col>
-        <Col span={4}>
-          <div className='Column'></div>
-          coluna 4
-        </Col>
-        <Col span={5}>
-          <div className='Column'></div>
-          coluna 5
+      <Row gutter={8} justify={'center'}>
+        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
+          <div className='Column'>coluna 1</div>
         </Col>
       </Row>
     </div>
