c23ab285998aefe05aa4e92ceb5753c1465dbb0c
Evandro Lima <evandro.lima@gat.digital>
2022-08-11 17:53:42 +0000

    Working with rows

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { DatePicker } from 'antd';
+import { Col, Row } from 'antd';
+import './index.css';
 
 function App() {
   return (
-    <div
-      className='App'
-      style={{ backgroundColor: 'white' }}
-    >
-      <DatePicker />
+    <div>
+      <Row gutter={16} justify={'center'} align={'middle'}>
+        <Col span={4}>
+          <div className='Column'></div>
+          coluna 1
+        </Col>
+        <Col span={4}>
+          <div className='Column'></div>
+          coluna 2
+        </Col>
+        <Col span={8}>
+          <div className='Column'></div>
+          coluna 3
+        </Col>
+        <Col span={4}>
+          <div className='Column'></div>
+          coluna 4
+        </Col>
+        <Col span={5}>
+          <div className='Column'></div>
+          coluna 5
+        </Col>
+      </Row>
     </div>
   );
 }
