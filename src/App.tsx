f240ccc9192c16d9bd0d75a14611bb2065e24dd5
Evandro Lima <evandro.lima@gat.digital>
2022-08-12 10:04:20 +0000

    Add table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import { Col, Row, Typography } from 'antd';
+import { Col, Row, Table } from 'antd';
+import { User, UserService } from 'goodvandro-alganews-sdk';
+import { useEffect, useState } from 'react';
 import './index.css';
 
-const { Title } = Typography;
-
 function App() {
+  const [users, setUsers] = useState<User.Summary[]>([]);
+
+  useEffect(() => {
+    UserService.getAllUsers().then(setUsers);
+  }, []);
+
   return (
     <div>
       <Row gutter={8} justify={'center'}>
-        <Col span={12}>
-          <Title>h1. Ant Design</Title>
-          <Title level={2}>h2. Ant Design</Title>
-          <Title level={3}>h3. Ant Design</Title>
-          <Title level={4}>h4. Ant Design</Title>
-          <Title level={5}>h5. Ant Design</Title>
+        <Col span={24}>
+          <Table
+            dataSource={users}
+            columns={[
+              {
+                title: 'Name',
+                dataIndex: 'name',
+              },
+              {
+                title: 'E-mail',
+                dataIndex: 'email',
+              },
+            ]}
+          />
         </Col>
       </Row>
     </div>
